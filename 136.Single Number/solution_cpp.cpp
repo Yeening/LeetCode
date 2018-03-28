e9996f9a7ef6d894ed23fe2a1a57128312f5af99
Eclear <eclearning@outlook.com>
2018-03-28 14:17:59 +0800

    Create solution_cpp.cpp

---
+++ 136.Single Number/solution_cpp.cpp
@@ -0,0 +1,28 @@
+
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        /*solution1: XOR*/
+        int res = 0;
+        for(int i = 0; i < nums.size(); i++){
+            res ^= nums[i];
+        }
+        return res;
+        /*solution 2: Math*/
+        /*set<int> nums_noredonant;
+        for(int i = 0; i < nums.size(); i++){
+            nums_noredonant.insert(nums[i]);
+        }
+        return 2 * accumulate(nums_noredonant.begin(),nums_noredonant.end(),0) - accumulate(nums.begin(),nums.end(),0);*/
+        /*solution3: Map*/
+        /*map<int,int> counts;
+        for(int i = 0; i < nums.size(); i++){
+            counts[nums[i]]++;
+        }
+        map<int,int>::iterator ite;
+        for(ite = counts.begin(); ite != counts.end(); ite++){
+            if( ite->second == 1 ) return ite->first;
+        }*/
+    }
+};
