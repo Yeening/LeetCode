b92f5308de36282660d171be042c4c35035b1a15
Eclear <eclearning@outlook.com>
2018-04-11 17:02:33 +0800

    Update cpp_solution.cpp

--- 033.Search_in_Rotated_Sorted_Array/cpp_solution.cpp
+++ 033.Search_in_Rotated_Sorted_Array/cpp_solution.cpp
@@ -2,9 +2,9 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        if(!nums.size()) return -1;
+        //if(!nums.size()) return -1;
         int low = 0, high = nums.size()-1;
-        while(low < high){
+        while(low <= high){
             int mid = (low + high)/2;
             if(nums[mid] == target) return mid;
             //first half in correct order
@@ -20,7 +20,7 @@ public:
             
         }
         //high equals to low
-        if(target == nums[high]) return high;
+        //if(target == nums[high]) return high;
         return -1;
     }
 };
