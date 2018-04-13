40816ce7f7474b0b8c0d3eda4aa92832e95c92e5
Eclear <eclearning@outlook.com>
2018-04-13 12:30:41 +0800

    Create cpp_solution.cpp

---
+++ 096.Unique_Binary_Search_Trees/cpp_solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        int * bsts = new int[n+1]();
+        bsts[0] = 1;
+        bsts[1] = 1;
+        bsts[2] = 2;
+        for(int i = 3; i <= n; i++){
+            for(int left = 0; left < i; left++){
+                bsts[i] = bsts[i] + bsts[left]*bsts[i-1-left];
+            }
+        }
+        return bsts[n];
+    }
+};
