9c6952b168020fb3336fd0c5584403ede75be643
Eclear <eclearning@outlook.com>
2018-04-17 11:14:38 +0800

    Create cpp_solution.cpp

---
+++ 022.Generate_Parentheses/cpp_solution.cpp
@@ -0,0 +1,16 @@
+//Concise recursive C++ solution
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        adding(res, "", n, 0);
+        return res;
+    }
+    void adding(vector<string> &res, string s, int left_remain, int right_toadd){
+        if(left_remain==0 && right_toadd ==0){
+            res.push_back(s);
+        }
+        if(left_remain > 0){ adding(res, s+"(", left_remain-1, right_toadd+1);}
+        if(right_toadd > 0){ adding(res, s+")", left_remain, right_toadd-1);}
+    }
+};
