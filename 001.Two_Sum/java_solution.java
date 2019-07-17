ed6b5c7c19622a1512c8cb3658afe7a4b051dbc3
Yeening Liu <eclearning@outlook.com>
2019-07-17 22:10:16 +0800

    Create java_solution.java

---
+++ 001.Two_Sum/java_solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<> ();
+        for(int i = 0; i < nums.length; i++){
+            int difference = target - nums[i];
+            if(map.containsKey(difference)){
+                return new int[]{map.get(difference), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
