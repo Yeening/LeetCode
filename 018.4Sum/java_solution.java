c28b77bb711b3793fab3442da92f670e8f50b0f4
Yeening Liu <eclearning@outlook.com>
2019-07-21 10:31:37 +0800

    Update java_solution.java

--- 018.4Sum/java_solution.java
+++ 018.4Sum/java_solution.java
@@ -27,3 +27,54 @@ class Solution {
         return res;
     }
 }
+
+
+//Improved recursive approach for k Sum(k>=2)
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        // List<List<Integer>> res = new LinkedList<>();
+        
+        return kSum(nums, target, 0, 4);
+    }
+    private ArrayList<List<Integer>> kSum(int[] nums, int target, int start, int k)
+    {
+        ArrayList<List<Integer>> res = new ArrayList<>();
+        int len = nums.length;
+        if(start > len - k || nums[start] * k > target || nums[len-1]*k < target) return res;
+        if(k==2){
+            int lo = start, hi = len-1;
+            while(lo<hi){
+                if(nums[lo] + nums[hi] == target){
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[lo]);
+                    temp.add(nums[hi]);
+                    res.add(temp);
+                    // res.add(Arrays.asList(nums[lo],nums[hi]));
+                    // This cannot work because the return type of Arrays.asList is Arrays$ArrayList, cannot be added with element
+                    while(lo < hi && nums[lo] == nums[lo+1]) lo++;
+                    while(hi > lo &&nums[hi-1] == nums[hi]) hi--;
+                    lo++;
+                    hi--;
+                }
+                else if(nums[lo] + nums[hi] < target) lo++;
+                else hi--;
+            }
+        }
+        else{
+            for(int i = start; i < len - k + 1; i++){
+                if(i > start && nums[i-1] == nums[i]) continue;
+                ArrayList<List<Integer>> temp = kSum(nums, target - nums[i], i + 1, k-1);
+                if(temp != null){
+                    for(List<Integer> t : temp){
+                        t.add(0, nums[i]);
+                    }   
+                    res.addAll(temp);
+                }                
+            }
+        
+
+        }
+        return res;
+    }
+}
