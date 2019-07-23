006924207e033a57127ac96ada029400f8899d1b
Yeening Liu <eclearning@outlook.com>
2019-07-23 19:49:29 +0800

    Create java_solution.java

---
+++ 021.Merge_Two_Sorted_Lists/java_solution.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+ //Iteractive solution
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode tail = head;
+        while(l1!=null || l2!=null){
+            if(l1 == null || (l2!=null && l1.val > l2.val)){
+                tail.next = l2;
+                tail = l2;
+                l2 = l2.next;
+            }
+            else if(l2 == null || (l1!=null&&l1.val <= l2.val)){
+                tail.next = l1;
+                tail = l1;
+                l1 = l1.next;
+            }
+        }
+        return head.next;
+    }
+}
+
+//Recursice solution
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        if(l1.val < l2.val){
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        else{
+            l2.next =  mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
