bbd37058fe87d3f437c26f183be54305cb5fd187
Yeening Liu <eclearning@outlook.com>
2019-09-04 00:06:35 -0400

    Update java_solution.java

--- 090.Subsets _II/java_solution.java
+++ 090.Subsets _II/java_solution.java
@@ -6,22 +6,14 @@ class Solution {
         Arrays.sort(nums);
         for(int i = 0; i < nums.length; i++){
             int size = res.size();
+            int j = 0;
             //For repreated nums
-            if(i > 0 && nums[i] ==nums[i-1]){
-                for(int j = start; j < size; j++){
-                    List<Integer> temp = new ArrayList<>(res.get(j));
-                    temp.add(nums[i]);
-                    res.add(temp);
-                }
-            }
-            //For new nums
-            else{
-                for(int j = 0; j < size; j++){
-                    List<Integer> temp = new ArrayList<>(res.get(j));
-                    temp.add(nums[i]);
-                    res.add(temp);
-                }                
-            }
+            if(i > 0 && nums[i] ==nums[i-1]) {j = start;}
+            for(; j < size; j++){
+                List<Integer> temp = new ArrayList<>(res.get(j));
+                temp.add(nums[i]);
+                res.add(temp);
+            }                
             start = size;
         }
         return res;
