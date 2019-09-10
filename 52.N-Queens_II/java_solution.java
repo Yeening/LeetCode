c8962194febdffa9ef3c3696cf538b503f328ace
Yeening Liu <eclearning@outlook.com>
2019-09-09 21:40:56 -0400

    Update java_solution.java

--- 52.N-Queens_II/java_solution.java
+++ 52.N-Queens_II/java_solution.java
@@ -19,3 +19,26 @@ class Solution {
         }
     }
 }
+//Avoid global variable
+class Solution {
+    // int count = 0;
+    public int totalNQueens(int n) {
+        //row index: y, 45-index: x + y + n, 135-index: x - y + 4*n - 1
+        boolean[] flag = new boolean[5*n];
+        solveNQueens(0, n, flag);
+        return solveNQueens(0, n, flag);
+    }
+    private int solveNQueens(int x, int n, boolean[] flag){
+        int count = 0;
+        if(x == n) count++;
+        //put queens on the positions of the row
+        for(int y = 0; y < n; y++){
+            if(!flag[y]&&!flag[x + y + n]&&!flag[x - y + 4*n - 1]){
+                flag[y] = flag[x+y+n] = flag[x-y+4*n-1] = true;
+                count += solveNQueens(x+1, n, flag);
+                flag[y] = flag[x+y+n] = flag[x-y+4*n-1] = false;
+            }
+        }
+        return count;
+    }
+}
