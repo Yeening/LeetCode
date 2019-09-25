fa883652e61052083c3e4d7dd6297970f62dc61b
Yeening Liu <eclearning@outlook.com>
2019-09-25 11:48:27 -0400

    Update java_solution.java

--- 169.Majority_Element/java_solution.java
+++ 169.Majority_Element/java_solution.java
@@ -1,6 +1,17 @@
 class Solution {
+    //Solution1: sorting
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
         return nums[nums.length/2];
     }
+    //Solution2: Moore voting
+    // public int majorityElement(int[] nums) {
+    //     int candidate = nums[0], count = 0;
+    //     for(int num: nums){
+    //         if(count==0) candidate = num;
+    //         if(num == candidate) count++;
+    //         else count--;
+    //     }
+    //     return candidate;
+    // }    
 }
