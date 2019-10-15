c96ff1f91c3071b22dc90a28d2fb74c6fc3d3194
Yeening Liu <eclearning@outlook.com>
2019-10-15 00:00:48 -0400

    Update java_solution.java

--- 035.Search_Insert_Position/java_solution.java
+++ 035.Search_Insert_Position/java_solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int lo = 0, hi = nums.length;
+        int lo = 0, hi = nums.length-1;
         while(lo<=hi){
             int mid = (lo + hi)/2;
             if(nums[mid] == target) return mid;
