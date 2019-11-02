ce6ab1598b67ce92fb92ae94888ca70722f41c89
Yeening Liu <eclearning@outlook.com>
2019-11-01 22:36:44 -0400

    Create java_solution.java

---
+++ 076.Minimum_Window_Substring/java_solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        int[] map = new int[128];
+        if(s.length()==0||s.length()<t.length()) return "";
+        for(int i = 0; i < t.length(); i++){
+            map[t.charAt(i)-'A'] ++;
+        }
+        int start = 0, end = 0, count = t.length();
+        int min_end = s.length()+1, min_start = 0;
+        while(end<s.length()){
+            if(map[s.charAt(end)-'A'] > 0) count--;
+            // if this char doesn't exist in t, the value in map will be negative
+            map[s.charAt(end)-'A']--;
+            end++;
+            // when the current start-end window is valid, move start to get short
+            while(count==0){
+                if(end - start < min_end - min_start){
+                    min_end = end;
+                    min_start = start;
+                }
+                map[s.charAt(start)-'A']++;
+                if(map[s.charAt(start)-'A']>0){
+                    count++;
+                }
+                start++;
+            }
+        }
+        if(min_end==s.length()+1) return "";
+        return s.substring(min_start, min_end);
+    }
+}
