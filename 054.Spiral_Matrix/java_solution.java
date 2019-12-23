a6cb86e43bcd6bcefbbd0e0838b7115f2af0d520
Yeening Liu <eclearning@outlook.com>
2019-12-23 18:31:36 +0800

    Update java_solution.java

--- 054.Spiral_Matrix/java_solution.java
+++ 054.Spiral_Matrix/java_solution.java
@@ -24,3 +24,36 @@ class Solution {
         return res;
     }
 }
+
+//Solution2: 100%
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        if(matrix.length==0||matrix[0].length==0) return new ArrayList<Integer>();
+        int m = matrix.length, n = matrix[0].length;
+        int resSize = m*n;
+        // Integer array, for following transfer to List of Integer
+        Integer[] resArray= new Integer[m*n];
+        int[][] move = {{1, 0}, {0, -1}, {-1, 0}, {0, 1}};
+        int index = 0;
+        //fill the first row
+        for(int x = 0; x < n; x++) resArray[index++] = matrix[0][x];
+        int i = 0, j = n-1; //start from top right
+        int direction = 0;
+        while(index < resSize){
+            for(int x = 0; x < m-1; x++){
+                i += move[direction][0];
+                j += move[direction][1];
+                resArray[index++] = matrix[i][j];                             
+            }
+            m--;
+            //Swap m and m
+            m ^= n;
+            n ^= m;
+            m ^= n;
+            direction = (direction+1)%4;            
+        }
+        // ArrayList<Integer> al1 = Arrays.asList(arr);
+        List<Integer> res = Arrays.asList(resArray);
+        return res;
+    }
+}
