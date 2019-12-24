25ee980269338c68c2f5ef4deeb54b24c50e7e6c
Yeening Liu <eclearning@outlook.com>
2019-12-24 16:11:34 +0800

    Create java_solution.java

---
+++ 042.Trapping_Rain_Water/java_solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int trap(int[] height) {
+        int leftMax = 0, rightMax = 0;
+        int i = 0, j = height.length-1;
+        int res = 0;
+        while(i < j){
+            leftMax = Math.max(height[i], leftMax);
+            rightMax = Math.max(height[j], rightMax);
+            if(leftMax < rightMax){
+                res += leftMax - height[i];
+                i++;
+            }
+            else{
+                res += rightMax - height[j];
+                j--;
+            }
+        }
+        return res;
+    }
+}
