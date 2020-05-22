8acd6b1efd4f21be44c4fcd83d17ef1032c05c2c
Yeening Liu <eclearning@outlook.com>
2020-05-22 23:27:35 +0800

    Update java_solution.java

--- 049.Group_Anagrams/java_solution.java
+++ 049.Group_Anagrams/java_solution.java
@@ -19,3 +19,23 @@ class Solution {
         return new ArrayList<>(map.values());
     }
 }
+
+//Solution2: make the O(Nlog(N)) sorting O(N)
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs==null||strs.length==0) return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s: strs){
+            char[] ca = new char[26];
+            for(char c: s.toCharArray()){
+                ca[c - 'a']++;
+            }
+            String cur = String.valueOf(ca);
+            if(!map.containsKey(cur)){
+                map.put(cur, new ArrayList<>());
+            }
+            map.get(cur).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
