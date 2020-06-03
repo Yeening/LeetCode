bd1f7236a7952e748be398341bd36066cf4d222e
Yeening Liu <eclearning@outlook.com>
2020-06-03 22:22:10 +0800

    Create java_solution.java

---
+++ Companies/Amazon/Longest_Substring_Without_Repeating_Characters/java_solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int[] map = new int[300];
+        char[] ca = s.toCharArray();
+        int max = 0, cur = 0, l = 0;
+        for(int r = 0; r < ca.length; r++){
+            while(map[ca[r]]!=0){
+                map[ca[l]]--;
+                l++;
+            }
+            map[ca[r]]++;
+            max = Math.max(r-l+1, max);
+        }
+        return max;
+    }
+}
