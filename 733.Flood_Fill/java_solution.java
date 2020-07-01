48e5c7616163fcc6578d8be61e3899a1cc013d23
Yeening Liu <eclearning@outlook.com>
2020-07-01 13:38:40 +0800

    Create java_solution.java

---
+++ 733.Flood_Fill/java_solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    int M;
+    int N;
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        M = image.length;
+        N = image[0].length;
+        fill(image, sr, sc, newColor, image[sr][sc]);
+        return image;
+    }
+
+    private void fill(int[][] image, int i, int j, int newColor, int oldColor){
+        if(i < 0 || i >=M || j<0 || j>=N || image[i][j]!=oldColor || image[i][j] == newColor) return;
+        image[i][j] = newColor;
+        fill(image, i-1, j, newColor, oldColor);
+        fill(image, i+1, j, newColor, oldColor);
+        fill(image, i, j-1, newColor, oldColor);
+        fill(image, i, j+1, newColor, oldColor);
+    }
+}
