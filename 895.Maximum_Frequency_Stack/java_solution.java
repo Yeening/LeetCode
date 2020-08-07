4d7f3ee9106012c91aeae5ce8440c1ff4b26035e
Yeening Liu <eclearning@outlook.com>
2020-08-07 13:30:43 +0800

    Create java_solution.java

---
+++ 895.Maximum_Frequency_Stack/java_solution.java
@@ -0,0 +1,37 @@
+class FreqStack {
+        Map<Integer, Integer> counts = new HashMap<>();
+        Map<Integer, Stack<Integer>> freqBuckets = new HashMap<>();
+        int maxFreq = 0;
+
+        public FreqStack() {
+
+        }
+
+        public void push(int x) {
+            int freq = counts.getOrDefault(x, 0)+1;
+            counts.put(x, freq);
+            maxFreq = Math.max(maxFreq, freq);
+            if(!freqBuckets.containsKey(freq)){
+                freqBuckets.put(freq, new Stack<>());
+            }
+            freqBuckets.get(freq).push(x);
+        }
+
+        public int pop() {
+            if(freqBuckets.get(maxFreq) == null || freqBuckets.get(maxFreq).isEmpty()) return -1;
+            Stack<Integer> maxBucket = freqBuckets.get(maxFreq);
+            int res = maxBucket.pop();
+            counts.put(res,counts.get(res)-1);
+            if(maxBucket.isEmpty()){
+                maxFreq--;
+            }
+            return res;
+        }
+    }
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack obj = new FreqStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ */
