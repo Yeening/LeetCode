44b0d539d55495cde527bf98db6f0d106b75a192
Yeening Liu <eclearning@outlook.com>
2020-08-20 14:20:21 +0800

    Update java_solution.java

--- 088.Merge_Sorted_Array/java_solution.java
+++ 088.Merge_Sorted_Array/java_solution.java
@@ -12,9 +12,6 @@ class Solution {
                 nums1[index--] = nums2[p2--];
             }
         }
-        while(p1 >= 0){
-            nums1[index--] = nums1[p1--];
-        }
         while(p2 >= 0){
             nums1[index--] = nums2[p2--];
         }
