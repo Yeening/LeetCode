48e7bc5d064b2520281cae729bef79caa9caea58
Yeening Liu <eclearning@outlook.com>
2020-09-24 13:23:17 +0800

    Update java_solution.java

--- 046.Permutations/java_solution.java
+++ 046.Permutations/java_solution.java
@@ -20,3 +20,32 @@ class Solution {
         }
     }
 }
+
+// By using swap, we can skip using list.contians or new a Set
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new LinkedList<>();
+        backTracking(nums, new ArrayList<>(), 0, res);
+        return res;
+    }
+    
+    private void backTracking(int[] nums, List<Integer> tmp, int start, List<List<Integer>> res){
+        if(tmp.size() == nums.length){
+            res.add(new ArrayList<Integer>(tmp));
+            return;
+        }
+        for(int i = start; i < nums.length; i++){
+            tmp.add(nums[i]);
+            swap(nums, i, start);
+            backTracking(nums, tmp, start+1, res);
+            tmp.remove(tmp.size()-1);
+            swap(nums, i, start);
+        }
+    }
+    
+    private void swap(int[] nums, int a, int b){
+        int tmp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = tmp;
+    }
+}
