282758ab8ac093ad7cffad815cae568f8ef57e36
Yeening Liu <eclearning@outlook.com>
2020-09-30 16:15:22 +0800

    Update java_solution.java

--- 075.Sort_Colors/java_solution.java
+++ 075.Sort_Colors/java_solution.java
@@ -1,3 +1,9 @@
+// This is a dutch partitioning problem. We are classifying the array into four groups: red, white, unclassified, and blue. 
+// Initially we group all elements into unclassified. We iterate from the beginning as long as the white pointer is less than the blue pointer.
+// If the white pointer is red (nums[white] == 0), we swap with the red pointer and move both white and red pointer forward. 
+// If the pointer is white (nums[white] == 1), the element is already in correct place, so we don't have to swap, 
+// just move the white pointer forward. If the white pointer is blue, we swap with the latest unclassified element.
+
 class Solution {
     public void sortColors(int[] nums) {
         if(nums==null || nums.length < 2) return;
