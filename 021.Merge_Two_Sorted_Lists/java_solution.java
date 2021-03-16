476d9c15c6a06336b2ea2f3da3ce798cc50b9dee
Yeening Liu <eclearning@outlook.com>
2021-03-16 21:47:07 +0800

    Update java_solution.java

--- 021.Merge_Two_Sorted_Lists/java_solution.java
+++ 021.Merge_Two_Sorted_Lists/java_solution.java
@@ -42,3 +42,27 @@ class Solution {
         }
     }
 }
+
+
+// Solution 3
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        ListNode head = new ListNode();
+        ListNode p = head;
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        p.next = mergeTwoLists(l1, l2);
+        return head.next;
+        
+    }
+}
