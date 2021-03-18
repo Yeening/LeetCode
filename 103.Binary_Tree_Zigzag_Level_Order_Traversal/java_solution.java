22cf83afc004ad139b1a66017745de769c5aaa67
Yeening Liu <eclearning@outlook.com>
2021-03-18 21:03:37 +0800

    Update java_solution.java

--- 103.Binary_Tree_Zigzag_Level_Order_Traversal/java_solution.java
+++ 103.Binary_Tree_Zigzag_Level_Order_Traversal/java_solution.java
@@ -10,22 +10,21 @@
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> res = new LinkedList<>();
-        if(root==null) return res;
         Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        int level = 1;
-        while(!queue.isEmpty()){
-            List<Integer> cur = new ArrayList<>();
-            int count = queue.size();
-            for(int i = 0; i < count; i++){
-                TreeNode node = queue.poll();
-                if(node.left!=null) queue.add(node.left);
-                if(node.right!=null) queue.add(node.right);
-                cur.add(node.val);
+        if (root != null) queue.add(root);
+        boolean RTL = false;
+        while (!queue.isEmpty()) {
+            LinkedList<Integer> level = new LinkedList<>();
+            int size = queue.size();
+            for (int i = 0 ; i < size; i++) {
+                TreeNode cur = queue.poll();
+                if (RTL) level.addFirst(cur.val);
+                else level.addLast(cur.val);
+                if(cur.left != null) queue.add(cur.left);
+                if(cur.right!= null) queue.add(cur.right);
             }
-            if(level%2==0) Collections.reverse(cur);
-            res.add(cur);
-            level++;
+            res.add(level);
+            RTL = !RTL;
         }
         return res;
     }
