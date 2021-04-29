77543a8beb7c24df525e767fa3951c68cbf52879
Yeening Liu <eclearning@outlook.com>
2021-04-29 20:55:14 +0800

    Update java_solution.java

--- 547.Number_of_Provinces/java_solution.java
+++ 547.Number_of_Provinces/java_solution.java
@@ -27,3 +27,38 @@ class Solution {
         }
     }
 }
+
+// Solution 2: Union find with path compress
+
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        if (isConnected == null || isConnected.length == 0 || isConnected[0].length == 0) return 0;
+        int N = isConnected.length;
+        int[] root = new int[N];
+        int province = N;
+        for (int i = 0 ; i < N; i++) {
+            root[i] = i;
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (isConnected[i][j] == 1) {
+                    int rootI = find(i, root);
+                    int rootJ = find(j, root);
+                    if (rootI != rootJ) {
+                        root[rootI] = root[rootJ];
+                        province--;
+                    }
+                }
+            }
+        }
+        return province;
+    }
+
+    private int find(int i, int[] root) {
+        while(root[i] != i) {
+            root[i] = root[root[i]];
+            i = root[i];
+        }
+        return i;
+    }
+}
