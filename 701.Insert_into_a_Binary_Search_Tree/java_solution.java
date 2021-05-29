73e7f37c95ee4e6e4cbe2a9d3392a052a08078a6
Yeening Liu <eclearning@outlook.com>
2021-05-29 17:21:20 +0800

    Create java_solution.java

---
+++ 701.Insert_into_a_Binary_Search_Tree/java_solution.java
@@ -0,0 +1,13 @@
+// Recursive
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            root = new TreeNode(val);
+        } else if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+}
