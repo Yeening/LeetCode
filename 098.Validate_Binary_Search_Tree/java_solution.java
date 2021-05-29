c49a246e86e93e1b1b7c36399ae1a30213907a22
Yeening Liu <eclearning@outlook.com>
2021-05-29 17:05:35 +0800

    Update java_solution.java

--- 098.Validate_Binary_Search_Tree/java_solution.java
+++ 098.Validate_Binary_Search_Tree/java_solution.java
@@ -13,6 +13,21 @@ class Solution {
     }
 }
 
+// Recursion without using long
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValid(root, null, null);
+    }
+
+    private boolean isValid(TreeNode root, Integer min, Integer max) {
+        if (root == null) return true;
+        if (min != null && root.val <= min ||
+                max != null && root.val >= max) return false;
+        return isValid(root.left, min, root.val) &&
+                isValid(root.right, root.val, max);
+    }
+}
+
 //Iterative in-order traverse
 class Solution {
     public boolean isValidBST(TreeNode root) {
