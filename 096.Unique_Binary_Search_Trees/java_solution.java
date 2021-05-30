beaa0082f8ceb19d685866fced1ff44c57207e5a
Yeening Liu <eclearning@outlook.com>
2021-05-30 22:22:52 +0800

    Update java_solution.java

--- 096.Unique_Binary_Search_Trees/java_solution.java
+++ 096.Unique_Binary_Search_Trees/java_solution.java
@@ -10,3 +10,29 @@ class Solution {
         return dp[n];
     }
 }
+
+// Solution 2: memo recursion
+class Solution {
+    int[][] memo;
+    int N;
+    public int numTrees(int n) {
+        N = n;
+        memo = new int[N][N];
+        return numTreesInterval(0, n-1);
+    }
+
+    private int numTreesInterval(int l, int r){
+        if (l > r) {
+            return 1;
+        }
+        if (memo[l][r] != 0) {
+            return memo[l][r];
+        }
+        int count = 0;
+        for(int i = l; i <= r; i++) {
+            count += numTreesInterval(l, i - 1) * numTreesInterval(i + 1, r);
+        }
+        memo[l][r] = count;
+        return count;
+    }
+}
