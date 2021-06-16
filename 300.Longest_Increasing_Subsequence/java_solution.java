a5d64423722e7d6627d0a6080c3934188500485c
Yeening Liu <eclearning@outlook.com>
2021-06-16 17:08:28 +0800

    Update java_solution.java

--- 300.Longest_Increasing_Subsequence/java_solution.java
+++ 300.Longest_Increasing_Subsequence/java_solution.java
@@ -1,5 +1,5 @@
 // Explanation: https://leetcode.com/problems/longest-increasing-subsequence/discuss/74824/JavaPython-Binary-search-O(nlogn)-time-with-explanation
-
+// Binary Search: O(N)
 class Solution {
     public int lengthOfLIS(int[] nums) {
         if(nums == null || nums.length==0) return 0;
@@ -27,4 +27,22 @@ class Solution {
         }
         return length;
     }
+}
+
+// DP: O(N^2)
+public int lengthOfLIS(int[] nums) {
+    if (nums == null || nums.length == 0) return 0;
+    int[] dp = new int[nums.length];
+    int res = 1;
+    dp[0] = 1;
+    for (int i = 1; i < nums.length; i++) {
+        dp[i] = 1;
+        for (int j = i - 1; j >= 0; j--) {
+            if (nums[j] < nums[i]) {
+                dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+        }
+        res = Math.max(res, dp[i]);
+    }
+    return res;
 }
