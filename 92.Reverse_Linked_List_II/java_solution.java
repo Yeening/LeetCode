be6e8bf6c964592ab8634d8499989c45bde1129e
Yeening Liu <eclearning@outlook.com>
2021-06-21 14:39:27 +0800

    Create java_solution.java

---
+++ 92.Reverse_Linked_List_II/java_solution.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (left == 1) return reverseFirstN(head, right);
+        head.next =  reverseBetween(head.next, left - 1, right - 1);
+        return head;
+    }
+
+    public ListNode reverseFirstN(ListNode head, int n) {
+        if (n <= 1) return head;
+        ListNode last = reverseFirstN(head.next, n - 1);
+        ListNode successor = head.next.next;
+        head.next.next = head;
+        head.next = successor;
+        return last;
+    }
+}
