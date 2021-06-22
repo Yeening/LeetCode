a45afbf5803cec07aa0e8a8d9e620c0c9f0965b3
Yeening Liu <eclearning@outlook.com>
2021-06-22 10:53:46 +0800

    Update java_solution.java

--- 092.Reverse_Linked_List_II/java_solution.java
+++ 092.Reverse_Linked_List_II/java_solution.java
@@ -27,3 +27,48 @@ class Solution {
         return last;
     }
 }
+
+// Iterative solution
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null) return head;
+        ListNode before = new ListNode(0, head), after = null,
+        l = head, r = null;
+        for (int i = 0; i < left - 1; i++){
+            l = l.next;
+            before = before.next;
+        }
+        r = l;
+        for (int i = left; i < right; i++) {
+            r = r.next;
+        }
+        after = r.next;
+
+        // reverse [left, right]
+        ListNode pre = before, cur = l;
+        while (cur != after) {
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        before.next.next = after;
+        before.next = pre;
+        // If left is 1, head will not point to the right node
+        if (left > 1) {
+            return head;
+        } else {
+            return before.next;
+        }
+    }
+}
