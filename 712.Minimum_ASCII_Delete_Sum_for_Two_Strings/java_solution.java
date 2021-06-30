9581a0dd6d155c664099d7f03cba6047b9ce3b5d
Yeening Liu <eclearning@outlook.com>
2021-06-30 22:17:16 +0800

    Update java_solution.java

--- 712.Minimum_ASCII_Delete_Sum_for_Two_Strings/java_solution.java
+++ 712.Minimum_ASCII_Delete_Sum_for_Two_Strings/java_solution.java
@@ -19,10 +19,9 @@ class Solution {
             for (int j = 1; j <= n; j++) {
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + text1.charAt(i - 1);
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
-                dp[i][j] = Math.max(dp[i][j],
-                        Math.max(dp[i - 1][j], dp[i][j - 1]));
-
             }
         }
         return dp[m][n];
