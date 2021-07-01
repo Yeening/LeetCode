6382a25291c9b0bee8275d808ec834024c3833a4
Yeening Liu <eclearning@outlook.com>
2021-07-01 15:34:52 +0800

    Create java_solution.java

---
+++ 518.Coin_Change_2/java_solution.java
@@ -0,0 +1,25 @@
+// Solution 1: DP, 7.27%, O(N * amount) space
+public static class Solution {
+    public int change(int amount, int[] coins) {
+        // dp[i][j]: combinations of coins[0, i-1] to amount j
+        // dp[0][...] = 0, dp[...][0] = 1
+        int n = coins.length;
+        int[][] dp = new int[n + 1][amount + 1];
+        for (int i = 1; i <= amount; i++) {
+            dp[0][i] = 0;
+        }
+        for(int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        // dp[i][j] = sum(dp[i-1][j - k * coins[i-1]]) k:[0, ...]
+        for (int i = 1; i <= n; i++) {
+            int cur = coins[i-1];
+            for (int j = 1; j <= amount; j++) {
+                for (int k = 0; cur * k <= j; k++) {
+                    dp[i][j] += dp[i-1][j - k * cur];
+                }
+            }
+        }
+        return dp[n][amount];
+    }
+}
