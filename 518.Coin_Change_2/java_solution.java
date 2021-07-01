0105a537d9ebb2f62f06103107f853bbaed9e1ec
Yeening Liu <eclearning@outlook.com>
2021-07-01 17:01:09 +0800

    Update java_solution.java

--- 518.Coin_Change_2/java_solution.java
+++ 518.Coin_Change_2/java_solution.java
@@ -23,3 +23,28 @@ public static class Solution {
         return dp[n][amount];
     }
 }
+
+// Solution 2: 46%
+class Solution {
+    public int change(int amount, int[] coins) {
+        // dp[i][j]: combinations of coins[0, i-1] to amount j
+        // dp[0][...] = 0, dp[...][0] = 1
+        int n = coins.length;
+        int[][] dp = new int[n + 1][amount + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i <= amount; i++) {
+            dp[0][i] = 0;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (j >= coins[i-1])
+                        dp[i][j] = dp[i-1][j] + dp[i][j - coins[i-1]];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][amount];
+    }
+}
