43c5ffcd098103008edef901a39301aaee48eb35
Yeening Liu <eclearning@outlook.com>
2021-07-01 14:59:34 +0800

    Update java_solution.java

--- 416.Partition_Equal_Subset_Sum/java_solution.java
+++ 416.Partition_Equal_Subset_Sum/java_solution.java
@@ -29,3 +29,38 @@ class Solution {
         return dp[n][w];
     }
 }
+
+// Solution 2: memo
+class Solution {
+    // 0: not-visit, 1 can fill; 2: cannot fill
+    int[][] memo;
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int i: nums) {
+            sum += i;
+        }
+        if (sum % 2 == 1) return false;
+        int target = sum / 2;
+        int n = nums.length;
+        memo = new int[n][target + 1];
+        return canFill(nums, n - 1, target);
+    }
+
+    // Can fill w with nums[0, n]
+    private boolean canFill(int[] nums, int n, int w) {
+        if (n < 0 || w < 0) return false;
+        if (memo[n][w] != 0) return memo[n][w] == 1;
+        if (w == 0){
+            memo[n][w] = 1;
+            return true;
+        }
+        if (nums[n] > w) return canFill(nums, n-1, w);
+        if (canFill(nums, n-1, w - nums[n]) || 
+                canFill(nums, n-1, w)) {
+            memo[n][w] = 1;
+        } else {
+            memo[n][w] = 2;
+        }
+        return memo[n][w] == 1;
+    }
+}
