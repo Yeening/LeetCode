e4b5eac1aca72751964953fd1515d4eb9072e504
Yeening Liu <eclearning@outlook.com>
2021-07-05 16:51:17 +0800

    Create java_solution.java

---
+++ 064.Minimum_Path_Sum/java_solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0) {
+                    if (j != 0) dp[i][j] = dp[i][j-1] + grid[i][j];
+                } else if (j == 0) {
+                    dp[i][j] = dp[i-1][j] + grid[i][j];
+                } else {
+                    dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
