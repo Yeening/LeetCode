b1c29acc42c5b9e4b930bd9fc505cd9ada9218db
Yeening Liu <eclearning@outlook.com>
2021-07-08 11:50:13 +0800

    Update java_solution.java

--- 081.Search_in_Rotated_Sorted_Array_II/java_solution.java
+++ 081.Search_in_Rotated_Sorted_Array_II/java_solution.java
@@ -1,5 +1,5 @@
-// a test case: [1,1,1,1,2,1,1,1,1,1]
-// with duplicates, we cannot check validation of a sequence by first and last number
+// a test case: [1,1,1,1,1,1,2,1,1,1,1,1]
+// Solution 1
 class Solution {
     public boolean search(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
@@ -32,5 +32,34 @@ class Solution {
         if (nums[mid] < minVal || nums[mid] > maxVal) return false;
         return isValid(nums, l, mid - 1, minVal, nums[mid]) && 
                 isValid(nums, mid + 1, r, nums[mid], maxVal);
+    }
+}
+
+// Solution 2: only check if nums[mid] == nums[left] == nums[right]
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left >> 1);
+            if (nums[mid] == target) return true;
+            if (nums[mid] == nums[left] && nums[mid] == nums[right]) {
+                left++;
+                right--;
+            }
+            else if (nums[right] >= nums[mid]) {
+                if (nums[mid] <= target && nums[right] >= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else if (nums[left] <= nums[mid]) {
+                if (nums[mid] >= target && nums[left] <= target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return false;
     }
 }
