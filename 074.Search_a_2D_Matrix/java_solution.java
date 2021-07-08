d3f1606393fa2491f86b8e53ab48ec89c95d37d3
Yeening Liu <eclearning@outlook.com>
2021-07-08 10:37:40 +0800

    Update java_solution.java

--- 074.Search_a_2D_Matrix/java_solution.java
+++ 074.Search_a_2D_Matrix/java_solution.java
@@ -1,3 +1,4 @@
+// Solution 1
 public boolean searchMatrix(int[][] matrix, int target) {
     int m = matrix.length, n = matrix[0].length;
     int i = 0, j = n - 1;
@@ -10,4 +11,24 @@ public boolean searchMatrix(int[][] matrix, int target) {
         }
     }
     return false;
+}
+
+// Solution 2
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int left = 0, right = m*n - 1;
+        while(left <= right) {
+            int mid = left + (right - left >> 1);
+            int i = mid / n, j = mid % n;
+            if (matrix[i][j] < target) {
+                left = mid + 1;
+            } else if (matrix[i][j] > target) {
+                right = mid - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
 }
