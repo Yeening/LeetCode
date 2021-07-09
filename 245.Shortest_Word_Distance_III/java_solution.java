323058ad2355bfa2ef51b0ba565726a73af89491
Yeening Liu <eclearning@outlook.com>
2021-07-09 16:24:24 +0800

    Create java_solution.java

---
+++ 245.Shortest_Word_Distance_III/java_solution.java
@@ -0,0 +1,20 @@
+public int shortestWordDistance(String[] wordsDict, String word1, String word2) {
+    int occ1 = -1, occ2 = -1, distance = wordsDict.length;
+    for (int i = 0 ; i < wordsDict.length; i++) {
+        if (wordsDict[i].equals(word1)) {
+            if (wordsDict[i].equals(word2) && occ1 >= 0) {
+                distance = Math.min(distance, i - occ1);
+            }
+            if (occ2 >= 0) {
+                distance = Math.min(distance, i - occ2);
+            }
+            occ1 = i;
+        } else if (wordsDict[i].equals(word2)) {
+            if (occ1 >= 0) {
+                distance = Math.min(distance, i - occ1);
+            }
+            occ2 = i;
+        }
+    }
+    return distance;
+}
