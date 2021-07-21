aec78123de85a054b79d80eb38f766c2eccb35cf
Yeening Liu <eclearning@outlook.com>
2021-07-21 17:17:07 +0800

    Update java_solution.java

--- 752.Open_the_Lock/java_solution.java
+++ 752.Open_the_Lock/java_solution.java
@@ -1,3 +1,4 @@
+// Solution 1: basic BFS
 class Solution {
     public int openLock(String[] deadends, String target) {
         Set<String> deadend = new HashSet<>(Arrays.asList(deadends));
@@ -35,5 +36,57 @@ class Solution {
             move++;
         }
         return -1;
+    }
+}
+
+// Solution 2: double direction BFS
+class Solution {
+    public int openLock(String[] deadends, String target) {
+        Set<String> visited = new HashSet<>(Arrays.asList(deadends));
+        Set<String> q1 = new HashSet<>();
+        Set<String> q2 = new HashSet<>();
+        q1.add("0000");
+        q2.add(target);
+        int step = 0;
+        while(!q1.isEmpty() && !q2.isEmpty()) {
+            if (q1.size() > q2.size()) {
+                Set<String> temp = q1;
+                q1 = q2;
+                q2 = temp;
+            }
+            Set<String> temp = new HashSet<>();
+            for (String cur: q1) {
+                if (visited.contains(cur)) continue;
+                visited.add(cur);
+                if (q2.contains(cur)) return step;
+                for (int i = 0 ; i < 4; i++) {
+                    String added = addOne(cur, i);
+                    if (!visited.contains(added)) {
+                        temp.add(added);
+                    }
+                    String minused = minusOne(cur, i);
+                    if (!visited.contains(minused)) {
+                        temp.add(minused);
+                    }
+                }
+            }
+            step++;
+            q1 = temp;
+        }
+        return -1;
+    }
+
+    private String addOne(String cur, int i) {
+        char[] ca = cur.toCharArray();
+        int c = ca[i] - '0';
+        ca[i] = (char) ('0' + (c + 1) % 10);
+        return new String(ca);
+    }
+
+    private String minusOne(String cur, int i) {
+        char[] ca = cur.toCharArray();
+        int c = ca[i] - '0';
+        ca[i] = (char) ('0' + (c + 9) % 10);
+        return new String(ca);
     }
 }
