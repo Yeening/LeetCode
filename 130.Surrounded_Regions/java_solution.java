af5f7b79e9daf41ba2c5a2ea4cb8c5230f34d0ec
Yeening Liu <eclearning@outlook.com>
2021-07-22 18:02:51 +0800

    Create java_solution.java

---
+++ 130.Surrounded_Regions/java_solution.java
@@ -0,0 +1,46 @@
+class Solution {
+    int m;
+    int n;
+    public void solve(char[][] board) {
+        m = board.length;
+        n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') {
+                dfs(board, i, 0);
+            }
+            if (board[i][n-1] == 'O') {
+                dfs(board, i, n-1);
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') {
+                dfs(board, 0, j);
+            }
+            if (board[m-1][j] == 'O') {
+                dfs(board, m-1, j);
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == '*') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int i, int j) {
+        if (i < 0 || i >= m || j < 0 || j >= n) {
+            return;
+        }
+        if (board[i][j] != 'O') return;
+        board[i][j] = '*';
+        dfs(board, i-1, j);
+        dfs(board, i+1, j);
+        dfs(board, i, j-1);
+        dfs(board, i, j+1);
+    }
+}
