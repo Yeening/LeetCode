d1b293b720051f9d167c031360b1ec8666f943c6
Yeening Liu <eclearning@outlook.com>
2021-07-23 17:39:12 +0800

    Update java_solution.java

--- 179.Largest_Number/java_solution.java
+++ 179.Largest_Number/java_solution.java
@@ -1,3 +1,4 @@
+// Solution 1: removing zero prefixes
 class Solution {
     public String largestNumber(int[] nums) {
         String[] numStrings = new String[nums.length];
@@ -18,6 +19,28 @@ class Solution {
         }
         if (zeroPrefixes == res.length()) return "0";
         return res.substring(zeroPrefixes);
+    }
+
+    private int compare(String num1, String num2) {
+        return (num2 + num1).compareTo(num1 + num2);
+    }
+}
+
+// Solution 2:
+class Solution {
+    public String largestNumber(int[] nums) {
+        String[] numStrings = new String[nums.length];
+        for (int i = 0 ; i < nums.length; i++) {
+            numStrings[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(numStrings, this::compare);
+        // if the largest number is 0, the whole sequence is 0
+        if (numStrings[0].equals("0")) return "0";
+        StringBuilder sb = new StringBuilder();
+        for (String numString: numStrings) {
+            sb.append(numString);
+        }
+        return sb.toString();
     }
 
     private int compare(String num1, String num2) {
