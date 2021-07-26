4beb1134d6311801aed54e645ba24be2891d5b61
Yeening Liu <eclearning@outlook.com>
2021-07-26 16:42:20 +0800

    Update java_solution.java

--- 986.Interval_List_Intersections/java_solution.java
+++ 986.Interval_List_Intersections/java_solution.java
@@ -1,3 +1,26 @@
+// Solution 1
+class Solution {
+    public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+        if (firstList.length == 0 || secondList.length == 0) return new int[][]{};
+        int i = 0, j = 0;
+        ArrayList<int[]> intersects = new ArrayList<>();
+        while (i < firstList.length && j < secondList.length) {
+            int lo = Math.max(firstList[i][0], secondList[j][0]);
+            int hi = Math.min(firstList[i][1], secondList[j][1]);
+            if (lo <= hi) {
+                intersects.add(new int[]{lo, hi});
+            }
+            if (firstList[i][1] < secondList[j][1]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return intersects.toArray(new int[][]{});
+    }
+}
+
+// Solution 2
 class Solution {
     public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
         if (firstList.length == 0 || secondList.length == 0) return new int[][]{};
