a1e1d4d5cc79f749ea2f4bae21c70a9574282091
Yeening Liu <eclearning@outlook.com>
2021-07-29 12:34:28 +0800

    Update java_solution.java

--- 015.3Sum/java_solution.java
+++ 015.3Sum/java_solution.java
@@ -33,3 +33,40 @@ class Solution {
         return res;
     }
 }
+
+// Solution 2
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new LinkedList<>();
+        if (nums.length < 3) return res;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; ) {
+            int cur = nums[i];
+            for (List<Integer> pair: twoSum(nums, i+1, -cur)) {
+                pair.add(cur);
+                res.add(pair);
+            }
+            while (i < nums.length && nums[i] == cur) i++;
+        }
+        return res;
+    }
+    
+    private List<List<Integer>> twoSum(int[] nums, int start, int target) {
+        List<List<Integer>> res = new LinkedList<>();
+        int l = start, r = nums.length - 1;
+        while (l < r) {
+            int lVal = nums[l], rVal = nums[r];
+            int sum = lVal + rVal;
+            if (sum < target) {
+                while (l < r && nums[l] == lVal) l++;
+            } else if (sum > target) {
+                while (r > l && nums[r] == rVal) r--;
+            } else if (sum == target){
+                res.add(new ArrayList<>(Arrays.asList(nums[l], nums[r])));
+                while (l < r && nums[l] == lVal) l++;
+                while (r > l && nums[r] == rVal) r--;
+            }
+        }
+        return res;
+    }
+}
