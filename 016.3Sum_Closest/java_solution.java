d4ac04aa2beb9ebbdcda150da55016374347a006
Yeening Liu <eclearning@outlook.com>
2021-07-29 14:29:09 +0800

    Update java_solution.java

--- 016.3Sum_Closest/java_solution.java
+++ 016.3Sum_Closest/java_solution.java
@@ -1,30 +1,23 @@
-//Sort + 2 pointers, 98%
-class Solution {
-    public int threeSumClosest(int[] nums, int target) {
-        if(nums==null||nums.length<3) return 0;
-        Arrays.sort(nums);
-        int res = nums[0] + nums[1] + nums[2];
-        int minDiff = Math.abs(res - target), lastI = nums[0];
-        for(int i = 0; i < nums.length - 2; i++){
-            if(i>0 && nums[i] == lastI) continue;
-            int l = i+1, r = nums.length-1;
-            int currentTarget = target - nums[i];
-            while(l < r){
-                int currentDiff = Math.abs(currentTarget - nums[l] - nums[r]);
-                if(currentDiff < minDiff){
-                    minDiff = currentDiff;
-                    res = nums[i] + nums[l] + nums[r];
-                }
-                if(nums[l] + nums[r] < currentTarget){
-                    l++;
-                }
-                else if(nums[l] + nums[r] > currentTarget){
-                    r--;
-                }
-                else return res;
+public int threeSumClosest(int[] nums, int target) {
+    int res = nums[0] + nums[1] + nums[2];
+    Arrays.sort(nums);
+    for (int i = 0 ; i < nums.length; i++) {
+        int first = nums[i];
+        int l = i + 1, r = nums.length - 1;
+        while (l < r) {
+            int lVal = nums[l], rVal = nums[r];
+            int sum = first + lVal + rVal;
+            if (Math.abs(sum - target) < Math.abs(res - target)) {
+                res = sum;
             }
-            lastI = nums[i];
+            if (sum < target) {
+                while (l < r && nums[l] == lVal) l++;
+            } else if (sum > target) {
+                while (l < r && nums[r] == rVal) r--;
+            } else if (sum == target) {
+                return target;
+            }
         }
-        return res;
     }
+    return res;
 }
