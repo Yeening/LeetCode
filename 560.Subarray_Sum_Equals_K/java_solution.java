41d5c61ba9996bf5c38b04bb6afb310111c2007d
Yeening Liu <eclearning@outlook.com>
2021-09-09 12:00:21 -0400

    Update java_solution.java

--- 560.Subarray_Sum_Equals_K/java_solution.java
+++ 560.Subarray_Sum_Equals_K/java_solution.java
@@ -1,10 +1,12 @@
+// From solution BF, we know the key to solve this problem is SUM[i, j]. 
+// So if we know SUM[0, i - 1] and SUM[0, j], then we can easily get SUM[i, j]. 
+// To achieve this, we just need to go through the array, calculate the current sum and save number of all seen PreSum to a HashMap.
 class Solution {
     // Time: O(n)
     // Space: O(n)
     public int subarraySum(int[] nums, int k) {
-        // ends with i, sum to j
-        HashMap<Integer, Integer> preSum = new HashMap<>(); // [{},{},{}]
-        preSum.put(0, 1); // [{1:1},{},{}]
+        HashMap<Integer, Integer> preSum = new HashMap<>();
+        preSum.put(0, 1); 
         int curSum = 0, res = 0;;
         for (int i = 0; i < nums.length; i++) {
             curSum += nums[i];
