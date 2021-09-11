fc7854f4cf3b7c5379fc8d95bbd635163f2acc57
Yeening Liu <eclearning@outlook.com>
2021-09-11 16:54:19 -0400

    Update java_solution.java

--- 1049.Last_Stone_Weight_II/java_solution.java
+++ 1049.Last_Stone_Weight_II/java_solution.java
@@ -29,3 +29,37 @@ class Solution {
         return Math.abs(sum - maxValue - maxValue);
     }
 }
+
+// could be optimized with range
+class Solution {
+    //  [1,1,4,2,3]
+    // sum = 11
+    // target = 6
+    
+    public int lastStoneWeightII(int[] stones) {
+        int sum = 0, N = stones.length;
+        for (int i: stones) {
+            sum += i; 
+        }
+        int target = sum % 2 == 0? sum >> 1: (sum >> 1) + 1;
+        // equals to a knap problem, bag space = target, elements = stones
+        // dp[i]: value of i is achieveable
+        boolean[] dp = new boolean[target+1];
+        dp[0] = true;
+        int range = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = Math.min(range, target); j >= 0; j--) {
+                if (dp[j] && j + stones[i] <= target) {
+                    dp[j + stones[i]] = true;
+                }
+            }
+            range += stones[i];
+        }
+        int maxValue = dp.length - 1;
+        while (maxValue >= 0) {
+            if (dp[maxValue]) break;
+            maxValue--;
+        }
+        return Math.abs(sum - maxValue - maxValue);
+    }
+}
