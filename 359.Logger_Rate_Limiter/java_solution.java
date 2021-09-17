6948bab53eb5d4ed21ab5808aa9998cbf6e9902d
Yeening Liu <eclearning@outlook.com>
2021-09-17 10:43:05 -0400

    Update java_solution.java

--- 359.Logger_Rate_Limiter/java_solution.java
+++ 359.Logger_Rate_Limiter/java_solution.java
@@ -13,3 +13,34 @@ class Logger{
     return print;
   }
 }
+
+
+// Optimize Space
+class Logger{
+
+    private Map<String, Integer> logLastPrint;
+    private int INTERVAL = 10;
+
+    public Logger(){
+        logLastPrint = new HashMap<>();
+    }
+
+    public boolean shouldPrint(String msg, int time) {
+        boolean print = time - logLastPrint.getOrDefault(msg, -INTERVAL) >= INTERVAL;
+        if (print) {
+            updateMap(time);
+        }
+        logLastPrint.put(msg, time);
+        return print;
+    }
+
+    private void updateMap(int time) {
+        Map<String, Integer> newMap = new HashMap<>();
+        for (Map.Entry<String, Integer> entry: logLastPrint.entrySet()) {
+            if (time - entry.getValue() < INTERVAL) {
+                newMap.put(entry.getKey(), entry.getValue());
+            }
+        }
+        logLastPrint = newMap;
+    }
+}
