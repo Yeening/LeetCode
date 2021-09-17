b3c2a6113704a4bb5df18058e71e71c7b66f2673
Yeening Liu <eclearning@outlook.com>
2021-09-17 15:41:31 -0400

    Update java_solution.java

--- 652.Find_Duplicate_Subtrees/java_solution.java
+++ 652.Find_Duplicate_Subtrees/java_solution.java
@@ -1,5 +1,8 @@
 // Solution: post-order
 // https://labuladong.gitbook.io/algo/mu-lu-ye-1/mu-lu-ye-1/er-cha-shu-xi-lie-3
+
+// time: O(Nodes^2)
+// space: O(Nodes)
 class Solution {
     private Map<String, Integer> subtree = new HashMap<>();
     private List<TreeNode> res = new LinkedList<>();
@@ -18,4 +21,37 @@ class Solution {
         }
         return cur;
     }
+}
+
+// Solution 2: using hashcode
+// time: O(Nodes)
+// space: O(Nodes)
+class Solution {
+    private Map<Integer, Integer> occurs; // {“null,4,null,”: 3, “null,4,null,2,null,”: 2, “null,4,null,2,null,3,null,4,null,”: 1, “null,4,null,2,null,1,null,4,null,2,null,3,null,4,null,”: 1}
+    private List<TreeNode> duplicates; // [4, 2]
+    private String SP = ",";
+
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        occurs = new HashMap<>();
+        duplicates = new ArrayList<>();
+        visit(root);
+        return duplicates;
+    }
+
+    private int visit(TreeNode root) {
+        if (root == null) return ("null" + SP).hashCode();
+        StringBuilder sb = new StringBuilder();
+        sb.append(visit(root.left));
+        sb.append(root.val);
+        sb.append(SP);
+        sb.append(visit(root.right));
+        sb.append(SP);
+        int id = sb.toString().hashCode();
+        if (occurs.containsKey(id) && occurs.get(id) == 1){
+            duplicates.add(root);
+        }
+        occurs.put(id, occurs.getOrDefault(id, 0) + 1);
+        return id;
+    }
+
 }
