a6e3ab1486b223914e92528ef3a4058d74fa7bc6
Yeening Liu <eclearning@outlook.com>
2021-09-18 11:22:43 -0400

    Update java_solution.java

--- 1277.Count_Square_Submatrices_with_All_Ones/java_solution.java
+++ 1277.Count_Square_Submatrices_with_All_Ones/java_solution.java
@@ -1,19 +1,5 @@
+// In-space dp, time: O(MN), space: O(1)
 class Solution {
-    /*
-    [
-    [0,1,1,1],
-    [1,1,1,1],
-    [0,1,1,1]]
-    
-    */
-    
-    /*
-    [
-    [0,0,0,0],
-    [0,0,0,0],
-    [0,0,0,1]]
-    
-    */
     public int countSquares(int[][] matrix) {
         int res = 0;
         for (int i = 0 ; i < matrix.length; i++){
@@ -27,6 +13,32 @@ class Solution {
             }
         }
         return res;
+    }
+
+}
+
+// memo, time: O(MN), space: O(MN)
+class Solution {
+    int[][] memo;
+
+    public int countSquares(int[][] matrix) {
+        int M = matrix.length, N = matrix[0].length;
+        int res = 0;
+        memo = new int[M][N];
+        for (int i = 0 ; i < M; i++){
+            for (int j = 0; j < N ; j++) {
+                res += squares(matrix, i, j);
+            }
+        }
+        return res;
+    }
+
+    private int squares(int[][] matrix, int i, int j) {
+        if (i > matrix.length - 1 || j > matrix[0].length - 1 ||                    matrix[i][j] == 0) return 0;
+        if (memo[i][j] != 0) return memo[i][j];
+        memo[i][j] = 1 + Math.min( squares(matrix, i + 1, j + 1),
+                Math.min(squares(matrix, i + 1, j), squares(matrix, i, j + 1)));
+        return memo[i][j];
     }
 
 }
