c2ba572f861facf4707a5e736a111b98471b5bb3
Yeening Liu <eclearning@outlook.com>
2021-09-24 14:36:20 -0400

    Update java_solution.java

--- 930.Binary_Subarrays_With_Sum/java_solution.java
+++ 930.Binary_Subarrays_With_Sum/java_solution.java
@@ -1,5 +1,7 @@
 // Using array is faster than hashmap
 // Record the presums
+// Time: O(N)
+// Space: O(N)
 class Solution {
     public int numSubarraysWithSum(int[] A, int S) {
         if(A == null || A.length == 0) return 0;
@@ -18,6 +20,33 @@ class Solution {
                 count += sums[presum - S];
             }
             sums[presum]++;
+        }
+        return count;
+    }
+}
+
+
+// Solution 2: modified silding windows
+// Time: O(N)
+// Space: O(1)
+class Solution {
+    // [0, 0, 0, 0, 0] goal = 0
+    //  l              r
+    // windowSum: 0
+    // count: 5
+    
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return numSubarraysMaxSum(nums, goal) - numSubarraysMaxSum(nums, goal - 1);
+    }
+    
+    private int numSubarraysMaxSum(int[] nums, int goal) {
+        int curSum = 0, l = 0, r = 0, count = 0;
+        while(r < nums.length) {
+            curSum += nums[r++];
+            while(l < r && curSum > goal) {
+                curSum -= nums[l++];
+            }
+            count += (r - l);
         }
         return count;
     }
