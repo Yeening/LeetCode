ab55889111a019ddeda7256f38288d98fa0929d7
Yeening Liu <eclearning@outlook.com>
2021-10-02 11:13:43 -0400

    Update java_solution.java

--- 647.Palindromic_Substrings/java_solution.java
+++ 647.Palindromic_Substrings/java_solution.java
@@ -1,4 +1,6 @@
 // dp with state compression
+// time: O(N^2)
+// space: O(N)
 class Solution {
     //  isPalindromic[i][j] == s[i] == s[j] && 
     //                (j<i+2 || isPalindromic[i+1][j-1])
@@ -18,5 +20,32 @@ class Solution {
             }
         }
         return palindromicCount;
+    }
+}
+
+// grow palindromic
+// time: O(N^2) but better than DP because it will not check every substring
+// space: O(1)
+class Solution {
+    //  isPalindromic[i][j] == s[i] == s[j] && 
+    //                (j<i+2 || isPalindromic[i+1][j-1])
+    int palindromicCount;
+    public int countSubstrings(String s) {
+        int n = s.length();
+        palindromicCount = 0;
+        for (int i = 0; i < n; i++) {
+            growSubstring(s, i, i);
+            growSubstring(s, i, i+1);
+        }
+        return palindromicCount;
+    }
+    
+    private void growSubstring(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && 
+               s.charAt(left) == s.charAt(right)) {
+            palindromicCount++;
+            left--;
+            right++;
+        }
     }
 }
