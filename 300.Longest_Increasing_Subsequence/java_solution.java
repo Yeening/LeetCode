f5d3f48d2133c593eba668c9d78dd9a00ef7dbca
Yeening Liu <eclearning@outlook.com>
2021-10-11 18:33:04 -0400

    Update java_solution.java

--- 300.Longest_Increasing_Subsequence/java_solution.java
+++ 300.Longest_Increasing_Subsequence/java_solution.java
@@ -1,5 +1,40 @@
 // Explanation: https://leetcode.com/problems/longest-increasing-subsequence/discuss/74824/JavaPython-Binary-search-O(nlogn)-time-with-explanation
-// Binary Search: O(N)
+// my binary search
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] lengthToTail = new int[nums.length + 1];
+        Arrays.fill(lengthToTail, Integer.MAX_VALUE-1);
+        lengthToTail[0] = Integer.MIN_VALUE;
+        int maxLength = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int left = 0, right = maxLength + 1;
+            // search range: [0, i], 
+            // invariant: largest x, lengthToTail[x] < nums[i] 
+            // in [left, right)
+            while (left < right - 1) {
+                int mid = left + (right - left >> 1);
+                if (lengthToTail[mid] >= nums[i]) {
+                    right = mid;
+                } else if (lengthToTail[mid] < nums[i]) {
+                    left = mid;
+                }
+            }// terminate: l = r - 1, mid = l, 
+            // lengthToTail[l] >= nums[i] or lengthToTail[l] < nums[i]
+            // l = r - 2, mid = l + 1
+            // lengthToTail[l + 1] < nums[i]
+            assert(lengthToTail[left] < nums[i]);
+            assert(left == nums.length - 1 || lengthToTail[left + 1] >= nums[i]);
+            int preLength = left;
+            int curLength = preLength + 1;
+            lengthToTail[curLength] = 
+                Math.min(lengthToTail[curLength], nums[i]);
+            maxLength = Math.max(maxLength, curLength);
+        }
+        return maxLength;
+    }
+}
+
+// Binary Search: O(NlogN)
 class Solution {
     public int lengthOfLIS(int[] nums) {
         if(nums == null || nums.length==0) return 0;
