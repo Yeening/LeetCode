31513794be4ed7746bb92c466ec5ed17d9325806
Yeening Liu <eclearning@outlook.com>
2021-10-17 22:35:46 -0400

    Update java_solution.java

--- 202.Happy_Number/java_solution.java
+++ 202.Happy_Number/java_solution.java
@@ -17,3 +17,48 @@ class Solution {
         return false;
     }
 }
+
+// solution 1, O(lgn) space
+class Solution {
+    // time: (lgn), space: (lgn)
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = next(n);
+        }
+        return n == 1;
+    }
+    
+    private int next(int n) {
+        int next = 0;
+        while (n > 0) {
+            next += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return next;
+    }
+}
+
+// solution 2, O(1) space, Floyd Cycle detection
+class Solution {
+    // time: (lgn), space: (lgn)
+    public boolean isHappy(int n) {
+        int fast = next(n), slow = n;
+        while (slow != fast && slow != 1 && fast != 1) {
+            fast = next(fast);
+            fast = next(fast);
+            slow = next(slow);
+        }
+        return slow == 1 || fast == 1;
+    }
+    
+    private int next(int n) {
+        int next = 0;
+        while (n > 0) {
+            next += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return next;
+    }
+}
