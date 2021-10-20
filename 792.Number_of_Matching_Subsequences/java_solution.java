d849b4f0a4ac66c471d50a3da4140b9796e82baa
Yeening Liu <eclearning@outlook.com>
2021-10-20 16:11:20 -0400

    Update java_solution.java

--- 792.Number_of_Matching_Subsequences/java_solution.java
+++ 792.Number_of_Matching_Subsequences/java_solution.java
@@ -1,3 +1,5 @@
+// solution 1: checking match for every word, 99ms
+// time: O(N*s.length()), N = words.length, space: O(N)
 class Solution {
     public int numMatchingSubseq(String s, String[] words) {
         Map<String, Integer> match = new HashMap<>();
@@ -30,5 +32,46 @@ class Solution {
             }
         }
         return wIndex == word.length();
+    }
+}
+
+// solution 2: keep a map of startChar:word, 37ms
+// time: O(N * s.length()), space: O(N)
+class Solution {
+    public int numMatchingSubseq(String s, String[] words) {
+        // int[]{wordIndex, startPos}
+        Queue<int[]>[] headQueues = new Queue[26]; // {a: [0,0], [2,0], [3,0], b:[1,0]}
+        int match = 0;
+        for (int i = 0; i < 26; i++) {
+            headQueues[i] = new ArrayDeque<>();
+        }
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            if (word.isEmpty()) {
+                match++;
+            } else {
+                char start = word.charAt(0);
+                headQueues[start - 'a'].offer(new int[]{i, 0});
+            }
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            Queue<int[]> curQueue = headQueues[cur - 'a'];
+            int size = curQueue.size();
+            for (int j = 0; j < size; j++) {
+                int[] wordRep = curQueue.poll();
+                String word = words[wordRep[0]];
+                int start = wordRep[1];
+                if (start == word.length() - 1) {
+                    match++;
+                } else {
+                    wordRep[1]++;
+                    headQueues[word.charAt(start + 1) - 'a'].offer(wordRep);
+                }
+            }
+        }
+        
+        return match;
     }
 }
