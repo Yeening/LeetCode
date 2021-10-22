7ff44d777bf0f17e4518e7ba37f6f7f17d9ece13
Yeening Liu <eclearning@outlook.com>
2021-10-21 23:36:18 -0400

    Update java_solution.java

--- 343.Integer_Break/java_solution.java
+++ 343.Integer_Break/java_solution.java
@@ -1,3 +1,4 @@
+// solution 1: dp
 class Solution {
     public int integerBreak(int n) {
         int[] maxProduct = new int[n+1];
@@ -9,5 +10,25 @@ class Solution {
             }
         }
         return maxProduct[n];
+    }
+}
+
+// solution 2: math, try to get 3 then 2
+class Solution {
+    public int integerBreak(int n) {
+        if (n < 4) {
+            int[] fixedValue = new int[]{0,1,1,2};
+            return fixedValue[n];
+        }
+        int remain3 = n % 3;
+        int count3 = n / 3;
+        if (remain3 == 0) {
+            return (int)Math.pow(3, count3);
+        } else if (remain3 == 1){
+            return (int)Math.pow(3, count3 - 1) * 4;
+        } else if (remain3 == 2) {
+            return (int)Math.pow(3, count3) * 2;
+        }
+        return 0;
     }
 }
