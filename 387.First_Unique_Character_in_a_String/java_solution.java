b618600ecd1a988a9c58586546cc8b56bdf8fa69
Yeening Liu <eclearning@outlook.com>
2021-11-08 10:14:40 -0500

    Update java_solution.java

--- 387.First_Unique_Character_in_a_String/java_solution.java
+++ 387.First_Unique_Character_in_a_String/java_solution.java
@@ -17,3 +17,32 @@ class Solution {
         return slow;
     }
 }
+
+// O(N)
+class Solution {
+    public int firstUniqChar(String s) {
+        // > -1: unique occur index; -1: not yet occur; -2: more than once
+        int[] charOccurs = new int[26];
+        int duplicate = 0; // optional, optimize for long sequence
+        Arrays.fill(charOccurs, -1);
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            if (charOccurs[cur - 'a'] == -2) {
+                continue;
+            } else if (charOccurs[cur - 'a'] > -1) {
+                charOccurs[cur - 'a'] = -2;
+                duplicate++;
+                if (duplicate == 26) {return -1;}
+            } else {
+                charOccurs[cur - 'a'] = i;
+            }
+        }
+        int first = s.length();
+        for (int occur: charOccurs) {
+            if (occur > -1) {
+                first = Math.min(first, occur);
+            }
+        }
+        return first == s.length()? -1: first;
+    }
+}
