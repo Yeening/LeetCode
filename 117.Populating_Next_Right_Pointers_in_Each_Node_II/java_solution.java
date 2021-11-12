24845e68a26573ad103028e5695024d0ae8a2893
Yeening Liu <eclearning@outlook.com>
2021-11-12 16:31:31 -0500

    Update java_solution.java

--- 117.Populating_Next_Right_Pointers_in_Each_Node_II/java_solution.java
+++ 117.Populating_Next_Right_Pointers_in_Each_Node_II/java_solution.java
@@ -21,37 +21,77 @@ class Node {
 };
 */
 
+// level order traverse, iterative
 class Solution {
     public Node connect(Node root) {
-        Node levelHead = null, prev = null, cur = root;
-        while(cur != null){
-            while(cur != null){
-                // Iterate the next level
-                if(cur.left != null){
-                    if(prev != null){
-                        prev.next = cur.left;
-                    }
-                    else{ // The first node of next level
-                        levelHead = cur.left;
-                    }
-                    prev = cur.left;
+        if (root == null) {return null;}
+        Node prev = null, nextLevelStart = null, cur = root;
+        while (cur != null) { // 2
+            if (cur.left != null) {
+                if (prev != null) {
+                    prev.next = cur.left;
+                } else {
+                    nextLevelStart = cur.left; // 2
                 }
-                if(cur.right != null){
-                    if(prev != null){
-                        prev.next = cur.right;
-                    }
-                    else{
-                        levelHead = cur.right;
-                    }
-                    prev = cur.right;
-                }
-                cur = cur.next;
+                prev = cur.left; // 2
             }
-            // go to next level
-            cur = levelHead;
-            levelHead = null;
-            prev = null;
+            
+            if (cur.right != null) {
+                if (prev != null) {
+                    prev.next = cur.right;
+                } else {
+                    nextLevelStart = cur.right;
+                }
+                prev = cur.right;
+            }
+            
+            if (cur.next != null) {
+                cur = cur.next;
+            } else {
+                cur = nextLevelStart;
+                prev = null;
+                nextLevelStart = null;
+            }
+            
         }
         return root;
+    }
+}
+
+// level order traverse, iterative 
+class Solution {
+    Set<Node> visited = new HashSet<>();
+    public Node connect(Node root) {
+        if (root == null) {return null;}
+        if (visited.contains(root)) {return root;}
+        visited.add(root);
+        if (root.left != null) {
+            if (root.right != null) {
+                root.left.next = root.right;
+            } else {
+                root.left.next = findNextLevelLeft(root.next);
+            }
+        }
+        if (root.right != null) {
+            root.right.next = findNextLevelLeft(root.next);
+        }
+        connect(root.next);
+        connect(root.left);
+        connect(root.right);
+        return root;
+    }
+    
+    
+    private Node findNextLevelLeft(Node root) {
+        while (root != null) {
+            if (root.left != null) {
+                return root.left;
+            } else if (root.right != null) {
+                return root.right;
+            } else {
+                root = root.next;
+            }
+        }
+        return null;
     }
 }
