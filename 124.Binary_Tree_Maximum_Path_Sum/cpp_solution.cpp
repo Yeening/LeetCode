a6a424c0e389da88c96b088f16fd9bd6d2de5f34
Yeening Liu <eclearning@outlook.com>
2023-09-04 15:34:53 -0700

    Update cpp_solution.cpp

--- 124.Binary_Tree_Maximum_Path_Sum/cpp_solution.cpp
+++ 124.Binary_Tree_Maximum_Path_Sum/cpp_solution.cpp
@@ -39,3 +39,43 @@ public:
        return max(left, right) + root->val;
     }
 };
+
+
+/**
+ * Solution 2: Dynamic Programming, using the TreeNode.val to store the dp value
+ */
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        int maxSum = root->val;
+        // Stores the max incomplete PathSum 
+        // that passes the node, or 0, whichever is bigger,
+        // in the TreeNode.val
+        visit(root, maxSum);
+        return maxSum;
+    }
+
+    /**
+     * Returns the maxPathSum that passes the node, 
+     * and is incomplete.
+     */
+    void visit(TreeNode* root, int& maxSum) {
+        if (root == nullptr) {
+            return;
+        }
+        if (root->left) {
+            visit(root->left, maxSum);
+        }
+        if (root->right) {
+            visit(root->right, maxSum);
+        }
+        int left = root->left? root->left->val: 0;
+        int right = root->right? root->right->val: 0;
+        int curSum = root->val + left + right;
+        maxSum = max(maxSum, curSum);
+        root->val = max(0, root->val + max(left, right));
+    }
+};
