3f201a4fc7680d200c990463ef5dc4a3bc86de57
Yeening Liu <eclearning@outlook.com>
2024-12-31 16:29:33 -0800

    Update java_solution.java

--- 190.Reverse_Bits/java_solution.java
+++ 190.Reverse_Bits/java_solution.java
@@ -10,3 +10,17 @@ public class Solution {
         return res;
     }
 }
+
+
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            int cur = n & 1;
+            res |= (cur << (32 - i - 1));
+            n = n >> 1;
+        }
+        return res;
+    }
+}
