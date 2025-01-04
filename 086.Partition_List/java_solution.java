dc57e5acc3c4c88d46b4820212fd3a51f84acba2
Yeening Liu <eclearning@outlook.com>
2025-01-03 23:02:08 -0800

    Create java_solution.java

---
+++ 086.Partition_List/java_solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode smallHead = new ListNode(-1), bigHead = new ListNode(-1);
+        ListNode small = smallHead, big = bigHead;
+        while (head != null) {
+            if (head.val < x) {
+                small.next = head;
+                small = small.next;
+            } else {
+                big.next = head;
+                big = big.next;
+            }
+            head = head.next;
+        }
+        small.next = bigHead.next;
+        big.next = null;
+        return smallHead.next;
+    }
+}
