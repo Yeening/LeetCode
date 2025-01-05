b0c39af4c997d30cbfa9a8a7b71b37245bb021d9
Yeening Liu <eclearning@outlook.com>
2025-01-04 19:14:13 -0800

    Update java_solution.java

--- 160.Intersection_of_Two_Linked_Lists/java_solution.java
+++ 160.Intersection_of_Two_Linked_Lists/java_solution.java
@@ -29,3 +29,40 @@ public class Solution {
         return p1;
     }
 }
+
+// Solution 2
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> visited = new HashSet<>();
+        ListNode p1 = headA, p2 = headB;
+        while (p1 != null && p2 != null) {
+            if (p1 == p2) {
+                // equal length
+                return p1;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        if (p2 == null) {
+            ListNode temp = headA;
+            headA = headB;
+            headB = temp;
+            p2 = p1;
+        }
+
+        // A is shorter than B
+        while (p2 != null) {
+            p2 = p2.next;
+            headB = headB.next;
+        }
+
+        while (headA != null) {
+            if (headA == headB) return headA;
+            headA = headA.next;
+            headB = headB.next;
+        }
+
+        return null;
+    }
+}
