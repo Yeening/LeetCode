281fc54be828a183f59e78fb4e933e44eeba2cd3
Yeening Liu <eclearning@outlook.com>
2025-02-17 11:23:32 -0800

    Update java_solution.java

--- 162.Find_Peak_Element/java_solution.java
+++ 162.Find_Peak_Element/java_solution.java
@@ -12,3 +12,28 @@ class Solution {
         return nums.length-1;
     }
 }
+
+
+// Solution 2: O(logn)
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int N = nums.length;
+        if (N == 1) return 0;
+        int l = 0, r = N - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            boolean greaterThanLeft = mid == 0 || nums[mid] > nums[mid-1];
+            boolean greaterThanRight = mid == N - 1 || nums[mid] > nums[mid + 1];
+            if (greaterThanLeft && greaterThanRight) {
+                return mid;
+            } else if (greaterThanLeft) {
+                l = mid + 1;
+            } else if (greaterThanRight) {
+                r = mid - 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return l;
+    }
+}
